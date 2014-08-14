4811fc05e37c7f4c76668af6261a0d23a5ba3338
Kleopatra <Kleopatra@Kleopatra-Slim>
2014-08-14 13:16:32 +0200

    - removed logging - updated doc

--- fx8-swingempire/src/java/de/swingempire/fx/scene/control/cell/XTableCellBehavior.java
+++ fx8-swingempire/src/java/de/swingempire/fx/scene/control/cell/XTableCellBehavior.java
@@ -17,11 +17,21 @@ import de.swingempire.fx.scene.control.XTableView;
 /**
  * Trying to intercept the selection process to not cancel an edit.
  * 
- * No luck: 
- * 
+ * Issues:
  * - the control of editing is centered completely inside the cell itself
  * - there's no way to get hold of the actual editing cell, it's only the
- * TablePosition of the cell that's available.
+ *   TablePosition of the cell that's available
+ * - on mousePressed (actually simpleSelect) this behaviour is playing
+ *   squash: the table is the wall, the target is the editing fellow cell
+ *   whose edit is rudely cancelled
+ * - with the current api, this behaviour can't do much better: table's
+ *   api is too weak, one method only  
+ * - hack here: add api to tableView that supports the notion of 
+ *   terminate an edit and use it (note: needs cooperation of all cells
+ *   in the table) 
+ * - astonished: _why_ the need to cancel (or otherwise end an edit anywhere
+ *   outside this cell) - shouldn't the selection/focus update automagically
+ *   trigger the other cell to update its editing?
  * 
  * @author Jeanette Winzenburg, Berlin
  */
@@ -34,21 +44,19 @@ public class XTableCellBehavior<S, T> extends TableCellBehavior<S, T>{
         super(control);
     }
 
-    @Override
-    public void mousePressed(MouseEvent event) {
-        LOG.info("pressed: " + getControl().isEditing());
-         
-        super.mousePressed(event);
-    }
+//    @Override
+//    public void mousePressed(MouseEvent event) {
+//        super.mousePressed(event);
+//    }
     
     
 
     @Override
     protected void simpleSelect(MouseEvent e) {
-        TableCell cell = getControl();
-        TableView table = cell.getTableColumn().getTableView();
+        TableCell<S, T> cell = getControl();
+        TableView<S> table = cell.getTableColumn().getTableView();
         if (table instanceof XTableView) {
-            ((XTableView) table).terminateEdit();
+            ((XTableView<S>) table).terminateEdit();
         }
         super.simpleSelect(e);
     }
