6ff428befa8ca1f396b863f22077416b528abbe9
kleopatra <fastegal@swingempire.de>
2015-12-10 13:56:13 +0100

    experimenting around bug in accelerator (in contextMenu)

---
+++ fx8-swingempire/src/demo/de/swingempire/fx/control/AcceleratorOnTab.java
@@ -0,0 +1,66 @@
+/*
+ * Created on 10.12.2015
+ *
+ */
+package de.swingempire.fx.control;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
+
+/**
+ * Acceleartors expected to work on current (focused/selected) tab
+ * http://stackoverflow.com/q/34198022/203657
+ * 
+ * it's a bug: 
+ * https://bugs.openjdk.java.net/browse/JDK-8088068
+ * 
+ * It's a rather crude bug:
+ * <li> all accelerators of all contextMenus are collected into a single
+ * Map (on the Scene) with the accelerator as key
+ * <li> implies that only the last installed is ever active
+ * <li> implies that actions from unrelated contextMenues can be invoke,
+ *  if the accelerator isn't in this
+ *  <li> doesn't help to disable (the formerly overridden are lost)
+ *  <li> install happens at the time of control.setContextMenu
+ *  <li> the workaround in the bug report handles duplicates, but not foreign
+ *  
+ */
+public class AcceleratorOnTab extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Tab t1 = new Tab("Tab 1");
+        TableView<Void> tv1 = new TableView<>();
+        t1.setContent(tv1);
+        MenuItem mi1 = new MenuItem("Action 1");
+        mi1.setAccelerator(KeyCombination.valueOf("F3"));
+        mi1.setOnAction(event -> System.out.println("Action 1!"));
+        ContextMenu ctx1 = new ContextMenu(mi1);
+        tv1.setContextMenu(ctx1);
+
+        Tab t2 = new Tab("Tab 2");
+        TableView<Void> tv2 = new TableView<>();
+        t2.setContent(tv2);
+        MenuItem mi2 = new MenuItem("Action 2");
+        mi2.setAccelerator(KeyCombination.valueOf("F3"));
+        mi2.setOnAction(event -> System.out.println("Action 2!"));
+        ContextMenu ctx2 = new ContextMenu(mi2);
+        tv2.setContextMenu(ctx2);
+
+        TabPane tabPane = new TabPane(t1, t2);
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+        primaryStage.setScene(new Scene(tabPane));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ fx8-swingempire/src/demo/de/swingempire/fx/control/AcceleratorOnText.java
@@ -0,0 +1,109 @@
+/*
+ * Created on 10.12.2015
+ *
+ */
+package de.swingempire.fx.control;
+
+import javafx.application.Application;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Control;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ * Original bug report: https://bugs.openjdk.java.net/browse/JDK-8088068
+ * 
+ */
+public class AcceleratorOnText extends Application {
+
+    Stage stage;
+
+    public void start(Stage primaryStage) {
+        this.stage = primaryStage;
+        TextArea ta1 = new TextArea();
+        TextArea ta2 = new TextArea();
+
+        MenuItem item1 = new MenuItem("Insert \"Hello\"");
+        item1.setOnAction((ActionEvent) -> ta1.setText("Hello"));
+        item1.setAccelerator(KeyCombination.keyCombination("Ctrl+I"));
+
+        MenuItem item2 = new MenuItem("Insert \"World!\"");
+        item2.setOnAction((ActionEvent) -> ta2.setText("World!"));
+        item2.setAccelerator(KeyCombination.keyCombination("Ctrl+I"));
+
+        ContextMenu context1 = new ContextMenu();
+        context1.getItems().add(item1);
+
+        ContextMenu context2 = new ContextMenu();
+        context2.getItems().add(item2);
+
+        ta1.setContextMenu(context1);
+        ta2.setContextMenu(context2);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().add(ta1);
+        vBox.getChildren().add(ta2);
+
+        Scene scene = new Scene(vBox, 300, 300);
+        // win
+        scene.addEventFilter(KeyEvent.KEY_PRESSED, this::handleShortcut);
+        // original
+//        scene.addEventFilter(KeyEvent.KEY_RELEASED, this::handleShortcut);
+        
+        primaryStage.setTitle("Accelerator Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /** workaround by Robert Lichtenberger:
+     *  handler that walks the contextMenu of the
+     *  focused node and looks for a matching accelerator.
+     *  If found, manually fires the item and consumes the event.
+     *  Installed as handler on the scene.
+     */
+    private void handleShortcut(KeyEvent e) {
+        Node n = findFocused(stage.getScene().getRoot());
+        n = stage.getScene().getFocusOwner();
+        if (n != null && n instanceof Control) {
+            Control c = (Control) n;
+            if (c.getContextMenu() != null) {
+                for (MenuItem item : c.getContextMenu().getItems()) {
+                    if (item.getAccelerator() != null
+                            && item.getAccelerator().match(e)) {
+                        item.fire();
+                        e.consume();
+                    }
+                }
+            }
+        }
+    }
+
+    public static Node findFocused(Node e) {
+        if (e instanceof Parent) {
+            Parent parent = (Parent) e;
+            for (Node node : parent.getChildrenUnmodifiable()) {
+                node = findFocused(node);
+                if (node != null)
+                    return node;
+            }
+        }
+        if (e.isFocusTraversable()) {
+            if (e.isFocused()) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
