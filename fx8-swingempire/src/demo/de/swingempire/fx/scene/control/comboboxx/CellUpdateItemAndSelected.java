1ffea19d2c9af009f708b598235facdbec829ee7
kleopatra <fastegal@swingempire.de>
2015-12-17 14:36:50 +0100

    further digging into cell.updateItem

---
+++ fx8-swingempire/src/demo/de/swingempire/fx/scene/control/comboboxx/CellUpdateItemAndSelected.java
@@ -0,0 +1,108 @@
+/*
+ * Created on 16.12.2015
+ *
+ */
+package de.swingempire.fx.scene.control.comboboxx;
+
+import java.util.logging.Logger;
+
+import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+/**
+ * http://stackoverflow.com/q/34316254/203657
+ * 
+ * selected cell without custom marker
+ * 
+ * PENDING JW: 
+ * <li>exact contract of updateItem? It's called only if the
+ * item/emptyness is changed (all concrete implementations), it's not called on
+ * changes to the state of the cell (f.i. its selected/focued properties) 
+ * <li>
+ * motivating example in cell api doc has color depending on isSelected - looks
+ * similar to this, 
+ * <li>core implementations that have additional state (f.i.
+ * CheckBoxXXCell) bind the state to a visual property of the view
+ * 
+ * This is a wrapper around the motivation api doc example of Cell. It contains
+ * conditional customization of text color in updateItem. Doesn't work in
+ * both directions:
+ * - selected item is not shown as white
+ * - sometimes, unselected item _is_ shown in white (select one, then scroll 
+ * down)
+ * 
+ */
+public class CellUpdateItemAndSelected extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Parent content = createContent();
+        Scene scene = new Scene(content, 400, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public Parent createContent() {
+        Pane content = new VBox(10);
+        ListView<Number> numberList = new ListView<>(createNumberData(20));
+        // this is the motivating example from cell's api doc
+        numberList.setCellFactory(cf -> {
+            ListCell<Number> cell = new ListCell<Number>() {
+
+                @Override
+                protected void updateItem(Number item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(item == null ? "" : item.toString());
+                    if (item != null) {
+                        double value = item.doubleValue();
+                        setTextFill(isSelected() ? Color.WHITE
+                                : value == 0 ? Color.BLACK
+                                        : value < 0 ? Color.RED : Color.GREEN);
+                        if (isSelected()) {
+                            LOG.info("selected index/item: " + getIndex() + " / " + item);
+                        }
+                    }
+                }
+
+            };
+            return cell;
+        });
+
+        Label selectedLabel = new Label();
+        selectedLabel.textProperty().bind(numberList.getSelectionModel().selectedIndexProperty().asString());
+        content.getChildren().addAll(numberList, selectedLabel);
+        return content;
+    }
+
+    protected ObservableList<Number> createNumberData(int count) {
+        ObservableList<Number> data = FXCollections.observableArrayList();
+        for (int i = -3; i < count; i++) {
+            data.add(i);
+        }
+        return data;
+    }
+
+    @SuppressWarnings("unused")
+    private static final Logger LOG = Logger
+            .getLogger(CellUpdateItemAndSelected.class.getName());
+}
--- fx8-swingempire/src/demo/de/swingempire/fx/scene/control/comboboxx/ComboBoxCellFactoryTest.java
+++ fx8-swingempire/src/demo/de/swingempire/fx/scene/control/comboboxx/ComboBoxCellFactoryTest.java
@@ -7,13 +7,19 @@ package de.swingempire.fx.scene.control.comboboxx;
 import java.util.logging.Logger;
 
 import javafx.application.Application;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -23,84 +29,132 @@ import javafx.util.Callback;
  * http://stackoverflow.com/q/34316254/203657
  * 
  * selected cell without custom marker
+ * 
+ * PENDING JW: 
+ * <li>exact contract of updateItem? It's called only if the
+ * item/emptyness is changed (all concrete implementations), it's not called on
+ * changes to the state of the cell (f.i. its selected/focued properties) 
+ * <li>
+ * motivating example in cell api doc has color depending on isSelected - looks
+ * similar to this, 
+ * <li>core implementations that have additional state (f.i.
+ * CheckBoxXXCell) bind the state to a visual property of the view
+ * 
  */
-public class ComboBoxCellFactoryTest extends Application
-{
-  public static void main(String[] args)
-  {
-    Application.launch(args);
-  }
+public class ComboBoxCellFactoryTest extends Application {
 
-  @Override
-  public void start(Stage stage)
-  {
-    Parent content = createContent();
-    Scene scene = new Scene(content, 400, 300);
-    stage.setScene(scene);    
-    stage.show();
-  }
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 
-  public Parent createContent()
-  {
-    FlowPane content = new FlowPane(10, 10);
+    @Override
+    public void start(Stage stage) {
+        Parent content = createContent();
+        Scene scene = new Scene(content, 400, 300);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-    ComboBox<String> combo = new ComboBox<String>();    
-//    ComboBoxX<String> combo = new ComboBoxX<String>();    
-    combo.setItems(FXCollections.observableArrayList("Item 1", "Item 2", "Item 3", "Item 4"));
-    combo.getSelectionModel().selectLast();
-    combo.setCellFactory(createCell(combo));
-    
-    ListView<String> list = new ListView<>();
-    list.setItems(combo.getItems());
-    list.setCellFactory(createCell(null));
-    content.getChildren().addAll(combo, list);
-    return content;
-  }
+    IntegerProperty created = new SimpleIntegerProperty();
+    public Parent createContent() {
+        Pane content = new VBox(10);
 
-protected Callback<ListView<String>, ListCell<String>> createCell(
-        ComboBox<String> combo) {
-    return new Callback<ListView<String>, ListCell<String>>()
-    {
-      @Override
-      public ListCell<String> call(ListView<String> p)
-      {
-        return new ListCell<String>()
-        {
-          private final Rectangle rectangle;
-          {
-            rectangle = new Rectangle(10, 10);
-            selectedProperty().addListener((c, ov, nv) -> {
-//                updateItem(getItem(), getItem() == null);
-            });
-          }
+        ComboBox<String> combo = new ComboBox<String>();
+        // ComboBoxX<String> combo = new ComboBoxX<String>();
+        combo.setItems(FXCollections.observableArrayList("Item 1", "Item 2")); // ,
+                                                                               // "Item 3",
+                                                                               // "Item 4"));
+        combo.getSelectionModel().selectLast();
+        combo.setCellFactory(createCell(combo));
 
-          @Override
-          protected void updateItem(String item, boolean empty)
-          {
-            super.updateItem(item, empty);
+        ListView<String> list = new ListView<>();
+        list.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        list.setItems(combo.getItems());
+        list.setCellFactory(createCell(null));
 
-            if (empty || item == null)
-            {
-              setText(null);
-              setGraphic(null);
+        ListView<Number> numberList = new ListView<>(createNumberData(100));
+        // this is the motivating example from cell's api doc
+        numberList.setCellFactory(cf -> {
+            ListCell<Number> cell = new ListCell<Number>() {
+
+                @Override
+                protected void updateItem(Number item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(item == null ? "" : item.toString());
+                    if (item != null) {
+                        double value = item.doubleValue();
+                        setTextFill(isSelected() ? Color.WHITE
+                                : value == 0 ? Color.BLACK
+                                        : value < 0 ? Color.RED : Color.GREEN);
+                        if (isSelected()) {
+                            LOG.info("selected: " + item);
+                        }
+                    }
+                }
+
+            };
+            created.set(created.get() + 1);
+            return cell;
+        });
+
+        Label createdLabel = new Label();
+        createdLabel.textProperty().bind(created.asString());
+        content.getChildren().addAll(numberList, createdLabel);
+        return content;
+    }
+
+    protected ObservableList<Number> createNumberData(int count) {
+        ObservableList<Number> data = FXCollections.observableArrayList(5, -1,
+                1, 7, 5, 8, 0, -10);
+        for (int i = 0; i < count; i++) {
+            data.add(i);
+        }
+        return data;
+    }
+
+    protected Callback<ListView<String>, ListCell<String>> createCell(
+            ComboBox<String> combo) {
+        return new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> p) {
+                return new ListCell<String>() {
+                    private final Rectangle rectangle;
+                    {
+                        rectangle = new Rectangle(10, 10);
+                        // Binding<Color> selectionColor =
+                        // Bindings.when(selectedProperty())
+                        // .then(Color.GREENYELLOW)
+                        // .otherwise(Color.RED);
+                        // rectangle.fillProperty().bind(selectionColor);
+                        selectedProperty().addListener((c, ov, nv) -> {
+                            // updateItem(getItem(), getItem() == null);
+                            });
+                    }
+
+                    @Override
+                    protected void updateItem(String item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        if (empty || item == null) {
+                            setText(null);
+                            setGraphic(null);
+                        } else {
+                            // boolean selected = combo.getValue().equals(item);
+                            boolean selected = isSelected();
+                            // boolean selected = isFocused();
+                            rectangle.setFill(selected ? Color.GREENYELLOW
+                                    : Color.RED);
+                            setGraphic(rectangle);
+                            setText(item);
+                            LOG.info("update: " + item + " / " + selected);
+                        }
+                    }
+                };
             }
-            else
-            {
-//              boolean selected = combo.getValue().equals(item);
-              boolean selected = isSelected();
-              rectangle.setFill(selected ? Color.GREENYELLOW : Color.RED);
-              setGraphic(rectangle);
-              setText(item);
-              LOG.info("update: " + item + " / " +selected);
-            }
-          }
         };
-      }
-    };
-}  
-  
-  @SuppressWarnings("unused")
-private static final Logger LOG = Logger
-        .getLogger(ComboBoxCellFactoryTest.class.getName());
-}
+    }
 
+    @SuppressWarnings("unused")
+    private static final Logger LOG = Logger
+            .getLogger(ComboBoxCellFactoryTest.class.getName());
+}
