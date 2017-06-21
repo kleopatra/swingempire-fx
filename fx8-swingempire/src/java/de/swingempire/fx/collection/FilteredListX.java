695d3c8f877470bb6495700dee111c7c19622e37
kleopatra <kleopatra@LAPTOP-51C3847J>
2017-06-21 09:14:39 +0200

    updated FilteredListX to jdk9: added getViewIndex
    added test on fine-grained notification of x list (failing)

--- fx8-swingempire/src/java/de/swingempire/fx/collection/FilteredListX.java
+++ fx8-swingempire/src/java/de/swingempire/fx/collection/FilteredListX.java
@@ -176,6 +176,11 @@ public final class FilteredListX<E> extends TransformationList<E, E>{
         return filtered[index];
     }
 
+    @Override
+    public int getViewIndex(int index) {
+        return Arrays.binarySearch(filtered, 0, size, index);
+    }
+
     private SortHelper getSortHelper() {
         if (helper == null) {
             helper = new SortHelper();
--- fx8-swingempire/src/test/de/swingempire/fx/collection/TransformationListTest.java
+++ fx8-swingempire/src/test/de/swingempire/fx/collection/TransformationListTest.java
@@ -351,6 +351,57 @@ public class TransformationListTest {
     /**
      * Expecting fine grained notification from filteredList
      * https://javafx-jira.kenai.com/browse/RT-39291
+     * https://bugs.openjdk.java.net/browse/JDK-8092288
+     * unchanged as of jdk9-ea-171
+     * 
+     * Here testing custom replacement
+     */
+    @Test
+//    @ConditionalIgnore (condition = IgnoreReported.class)
+    public void testFilteredListXOneFiltered() {
+        ObservableList<String> list = createObservableList(true);
+        FilteredListX filtered = new FilteredListX(list, p -> true);
+        ListChangeReport report = new ListChangeReport(filtered);
+        // keep all except the third, equivalent to removing one item
+        filtered.setPredicate(p-> p != list.get(2));
+        Change c = report.getLastChange();
+        c.next();
+        assertTrue("expected: single remove but was: " + c, wasSingleRemoved(c));
+    }
+    
+    /**
+     * Expecting fine grained notification from filteredList
+     * https://javafx-jira.kenai.com/browse/RT-39291
+     * https://bugs.openjdk.java.net/browse/JDK-8092288
+     * unchanged as of jdk9-ea-171
+     * 
+     * Here testing custom replacement
+     */
+    @Test
+//    @ConditionalIgnore (condition = IgnoreReported.class)
+    public void testFilteredXList() {
+        ObservableList<String> list = createObservableList(true);
+        // needs a predicate
+//        FilteredList<String> sorted = list.filtered();
+        FilteredListX filtered = new FilteredListX(list);
+        List added = new ArrayList();
+        for (int i = 1; i < list.size(); i +=2) {
+            added.add(list.get(i));
+        }
+        
+        ListChangeReport report = new ListChangeReport(filtered);
+        filtered.setPredicate(p -> added.contains(p));
+        prettyPrint(report.getLastChange());
+        assertEquals(1, report.getEventCount());
+        // unexpected: filtering fires a single replaced
+        assertEquals("disjoint removes", added.size(), getChangeCount(report.getLastChange()));
+    }
+    
+    /**
+     * Expecting fine grained notification from filteredList
+     * https://javafx-jira.kenai.com/browse/RT-39291
+     * https://bugs.openjdk.java.net/browse/JDK-8092288
+     * unchanged as of jdk9-ea-171
      */
     @Test
     @ConditionalIgnore (condition = IgnoreReported.class)
@@ -368,6 +419,8 @@ public class TransformationListTest {
     /**
      * Expecting fine grained notification from filteredList
      * https://javafx-jira.kenai.com/browse/RT-39291
+     * https://bugs.openjdk.java.net/browse/JDK-8092288
+     * unchanged as of jdk9-ea-171
      */
     @Test
     @ConditionalIgnore (condition = IgnoreReported.class)
