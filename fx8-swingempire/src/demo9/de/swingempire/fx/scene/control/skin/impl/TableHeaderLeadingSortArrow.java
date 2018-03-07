35c1ec9cfd44375e2bd6c60ae2e4a4098b71c270
kleopatra <kleopatra@LAPTOP-51C3847J>
2018-03-07 12:49:25 +0100

    TableColumnHeader with leading sort indicator - made styleable

--- fx8-swingempire/src/demo9/de/swingempire/fx/scene/control/skin/impl/TableHeaderLeadingSortArrow.java
+++ fx8-swingempire/src/demo9/de/swingempire/fx/scene/control/skin/impl/TableHeaderLeadingSortArrow.java
@@ -4,17 +4,28 @@
  */
 package de.swingempire.fx.scene.control.skin.impl;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.logging.Logger;
 
 import de.swingempire.fx.util.FXUtils;
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
 import javafx.collections.FXCollections;
+import javafx.css.CssMetaData;
+import javafx.css.Styleable;
+import javafx.css.StyleableObjectProperty;
+import javafx.css.StyleableProperty;
+import javafx.css.converter.EnumConverter;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.Skin;
 import javafx.scene.control.TableColumn;
@@ -53,6 +64,7 @@ public class TableHeaderLeadingSortArrow extends Application {
             Node sortArrow = getSortArrow();
             // no sort indicator, nothing to do
             if (sortArrow == null || !sortArrow.isVisible()) return;
+            if (getSortIconDisplay() == ContentDisplay.RIGHT) return;
             // re-arrange label and sort indicator
             double sortWidth = sortArrow.prefWidth(-1);
             double headerWidth = snapSizeX(getWidth()) - (snappedLeftInset() + snappedRightInset());
@@ -66,6 +78,87 @@ public class TableHeaderLeadingSortArrow extends Application {
             getLabel().resizeRelocate(sortWidth, 0, headerWidth - sortWidth, getHeight());
         }
         
+        // --------------- make sort icon location styleable
+        // default value (strictly speaking: an implementation detail)
+        // PENDING: what about RtoL orientation? Is it handled correctly in core?
+        private static final ContentDisplay DEFAULT_SORT_ICON_DISPLAY = ContentDisplay.RIGHT;
+        private ObjectProperty<ContentDisplay> sortIconDisplay; 
+        
+        private ContentDisplay getSortIconDisplay() {
+            return sortIconDisplay == null ? DEFAULT_SORT_ICON_DISPLAY : sortIconDisplay.get();
+        }
+        
+        private ObjectProperty<ContentDisplay> sortIconProperty() {
+            if (sortIconDisplay == null) {
+                sortIconDisplay = new StyleableObjectProperty<>(DEFAULT_SORT_ICON_DISPLAY) {
+
+                    @Override
+                    public CssMetaData<? extends Styleable, ContentDisplay> getCssMetaData() {
+                        return StyleableProperties.SORT_ICON_DISPLAY;
+                    }
+
+                    @Override
+                    public Object getBean() {
+                        return MyTableColumnHeader.this;
+                    }
+
+                    @Override
+                    public String getName() {
+                        return "sortIconDisplay";
+                    }
+                    
+                };
+                
+            }
+            return sortIconDisplay;
+        }
+        
+        private static class StyleableProperties {
+            private static final CssMetaData<MyTableColumnHeader,ContentDisplay> SORT_ICON_DISPLAY =
+                    new CssMetaData<MyTableColumnHeader, ContentDisplay>("-fx-sort-icon-display",
+                         new EnumConverter<ContentDisplay>(ContentDisplay.class), DEFAULT_SORT_ICON_DISPLAY) {
+
+                    @Override
+                    public boolean isSettable(MyTableColumnHeader columnHeader) {
+                        return columnHeader.sortIconDisplay == null || !columnHeader.sortIconDisplay.isBound();
+                    }
+
+                    @Override
+                    public StyleableProperty<ContentDisplay> getStyleableProperty(MyTableColumnHeader n) {
+                        return (StyleableProperty<ContentDisplay>) n.sortIconProperty();
+                    }
+                };
+
+                 private static final List<CssMetaData<? extends Styleable, ?>> STYLEABLES;
+                 static {
+
+                    final List<CssMetaData<? extends Styleable, ?>> styleables =
+                        new ArrayList<CssMetaData<? extends Styleable, ?>>(TableColumnHeader.getClassCssMetaData());
+                    styleables.add(SORT_ICON_DISPLAY);
+                    STYLEABLES = Collections.unmodifiableList(styleables);
+
+                 }
+
+        }
+
+        /**
+         * Returnst the CssMetaData associated with this class, which may include the
+         * CssMetaData of its superclasses.
+         * @return the CssMetaData associated with this class, which may include the
+         * CssMetaData of its superclasses
+         */
+        public static List<CssMetaData<? extends Styleable, ?>> getClassCssMetaData() {
+            return StyleableProperties.STYLEABLES;
+        }
+
+        /** {@inheritDoc} */
+        @Override 
+        public List<CssMetaData<? extends Styleable, ?>> getCssMetaData() {
+            return getClassCssMetaData();
+        }
+
+        
+//-------- reflection acrobatics .. might use lookup and/or keeping aliases around
         private Node getSortArrow() {
             return (Node) FXUtils.invokeGetFieldValue(TableColumnHeader.class, this, "sortArrow");
         }
@@ -96,6 +189,7 @@ public class TableHeaderLeadingSortArrow extends Application {
         table.getColumns().addAll(countryCode, language, variant);
         
         BorderPane pane = new BorderPane(table);
+        
         return pane;
     }
 
@@ -146,6 +240,8 @@ public class TableHeaderLeadingSortArrow extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(new Scene(createContent()));
+        URL uri = getClass().getResource("columnheader.css");
+        stage.getScene().getStylesheets().add(uri.toExternalForm());
         stage.setTitle(FXUtils.version());
         stage.show();
     }
