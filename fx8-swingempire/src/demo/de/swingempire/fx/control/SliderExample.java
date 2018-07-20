85b338a88590b436dca2d2d46b06b8f90c115170
kleopatra <kleopatra@192.168.1.245>
2018-07-20 12:21:37 +0200

    exp around slider - old prob with correlated properties

--- fx8-swingempire/src/demo/de/swingempire/fx/control/SliderExample.java
+++ fx8-swingempire/src/demo/de/swingempire/fx/control/SliderExample.java
@@ -4,10 +4,14 @@
  */
 package de.swingempire.fx.control;
 
-import de.swingempire.fx.scene.control.slider.XSliderSkin;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Skin;
 import javafx.scene.control.Slider;
+import javafx.scene.control.skin.SliderSkin;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
@@ -16,16 +20,56 @@ import javafx.stage.Stage;
  * 
  * http://stackoverflow.com/q/35129670/203657
  * 
- * seems to be working in fx9
+ * seems to be working in fx9 - no, none of the listeners is reliable:
+ * - listening to isValueChanging is fine for the boundary values, incorrect for
+ *   intermediate (returns the value before snapping)
+ * - listening to value (either via Invalidation or Change) isn't notified
+ *   on boundary but gives correct values in-between  
+ * 
+ * new question: https://stackoverflow.com/q/51089812/203657
  * 
  * @author Jeanette Winzenburg, Berlin
  */
 public class SliderExample extends Application {
     public static void main(String[] args) { launch(args); }
 
+    public static class MSliderSkin extends SliderSkin {
+
+        Node thumbAlias;
+        /**
+         * Idea: revert order in thumb.mouseReleased - done in behaviour, which is final
+         * field, no way. But then: do the handling manually in custom mouseReleased handler
+         * 
+         * doesn't help: now we don't get notification on value with state !changing
+         * (because we are only interested in those)
+         * 
+         */
+        public MSliderSkin(Slider control) {
+            super(control);
+            thumbAlias = control.lookup(".thumb");
+            thumbAlias.setOnMouseReleased(this::thumbReleased);
+        }
+        
+        private void thumbReleased(MouseEvent me) {
+            final Slider slider = getSkinnable();
+            // RT-15207 When snapToTicks is true, slider value calculated in drag
+            // is then snapped to the nearest tick on mouse release.
+            slider.adjustValue(slider.getValue());
+            slider.setValueChanging(false);
+
+        }
+        
+    }
     @Override
     public void start(Stage primaryStage) {
-        Slider slider = new Slider(0.25, 2.0, 1.0);
+        Slider slider = new Slider(0.25, 2.0, 1.0) {
+
+//            @Override
+//            protected Skin<?> createDefaultSkin() {
+//                return new MSliderSkin(this);
+//            }
+            
+        };
 //        slider.setSkin(new XSliderSkin(slider));
         slider.setShowTickLabels(true);
         slider.setShowTickMarks(true);
@@ -36,14 +80,20 @@ public class SliderExample extends Application {
         
         slider.valueProperty().addListener( n -> {
             if (!slider.isValueChanging()) {
-                System.err.println(n);
+//                System.err.println("valueInvalid: " + ((ObservableValue) n).getValue());
             }
         });
 
+        slider.valueProperty().addListener((src, ov, nv) -> {
+            if (!slider.isValueChanging()) {
+              System.err.println("valueChange: " + nv);
+          }
+            
+        });
         slider.valueChangingProperty().addListener( (prop, oldVal, newVal) -> {
             // NOT the final value when newVal == false!!!!!!!
             if (!newVal) {
-                System.err.println(prop + "/" + oldVal + "/" + newVal); 
+//                System.err.println("changing: " + "/" + oldVal + "/" + newVal); 
                 System.err.println("committed value: " + slider.getValue());
             }
          });
