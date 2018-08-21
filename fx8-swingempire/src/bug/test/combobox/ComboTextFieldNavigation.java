755f6a6e4ca76d21fff33d7d4ec6fb9def6ddd95
kleopatra <kleopatra@192.168.1.245>
2018-08-21 13:21:01 +0200

    bug report: left/right not working in editor of combo

--- fx8-swingempire/src/bug/test/combobox/ComboTextFieldNavigation.java
+++ fx8-swingempire/src/bug/test/combobox/ComboTextFieldNavigation.java
@@ -4,6 +4,7 @@
  */
 package test.combobox;
 
+import java.time.LocalDate;
 import java.util.logging.Logger;
 
 import javafx.application.Application;
@@ -11,12 +12,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
-import javafx.scene.control.skin.ComboBoxListViewSkin;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
+ * Regression: left/right keys not working in editor
+ * reported: https://bugs.openjdk.java.net/browse/JDK-8209788
+ * 
  * https://stackoverflow.com/q/51943654/203657
  * ctrl-A not working if dropDown showing
  * 
@@ -36,7 +39,10 @@ public class ComboTextFieldNavigation extends Application {
                 "Seven", "Eight", "Nine", "Ten");
         ComboBox<String> cb = new ComboBox<String>(items);
         cb.setEditable(true);
-        root.getChildren().addAll(cb);
+        
+        DatePicker picker = new DatePicker(LocalDate.now());
+        
+        root.getChildren().addAll(cb, picker);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
