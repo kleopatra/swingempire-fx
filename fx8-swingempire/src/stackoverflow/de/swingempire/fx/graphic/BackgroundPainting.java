c424e2f31470afb3e6e6f2d6ecb261709bfd987c
kleopatra <fastegal@swingempire.de>
2019-11-17 13:57:56 +0100

    just formatting

--- fx8-swingempire/src/stackoverflow/de/swingempire/fx/graphic/BackgroundPainting.java
+++ fx8-swingempire/src/stackoverflow/de/swingempire/fx/graphic/BackgroundPainting.java
@@ -88,24 +88,26 @@ public class BackgroundPainting extends Application {
     }
 
     /**
+     * Code from https://stackoverflow.com/a/51919485/203657 
      * copy the given image to a writeable image
+     * 
      * @param image
      * @return a writeable image
      */
     public static WritableImage copyImage(Image image) {
-      int height=(int)image.getHeight();
-      int width=(int)image.getWidth();
-      PixelReader pixelReader=image.getPixelReader();
-      WritableImage writableImage = new WritableImage(width,height);
-      PixelWriter pixelWriter = writableImage.getPixelWriter();
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+        PixelReader pixelReader = image.getPixelReader();
+        WritableImage writableImage = new WritableImage(width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
 
-      for (int y = 0; y < height; y++){
-          for (int x = 0; x < width; x++){
-              Color color = pixelReader.getColor(x, y);
-              pixelWriter.setColor(x, y, color);
-          }
-      }
-      return writableImage;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color color = pixelReader.getColor(x, y);
+                pixelWriter.setColor(x, y, color);
+            }
+        }
+        return writableImage;
     }
 
 
