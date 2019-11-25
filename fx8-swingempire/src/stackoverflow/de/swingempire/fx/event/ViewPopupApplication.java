2b3ec948bd9900b0339d7d1d6cb778a6e38029b1
kleopatra <fastegal@swingempire.de>
2019-11-25 13:18:14 +0100

    example of custom event dispatcher

--- fx8-swingempire/src/stackoverflow/de/swingempire/fx/event/ViewPopupApplication.java
+++ fx8-swingempire/src/stackoverflow/de/swingempire/fx/event/ViewPopupApplication.java
@@ -4,18 +4,26 @@
  */
 package de.swingempire.fx.event;
 
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
 import java.util.logging.Logger;
 
-import static javafx.scene.input.KeyCode.*;
-import static javafx.scene.input.KeyEvent.*;
+import com.sun.javafx.event.BasicEventDispatcher;
 
 import de.swingempire.fx.util.FXUtils;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.event.Event;
 import javafx.event.EventDispatchChain;
+import javafx.event.EventDispatcher;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Popup;
@@ -28,51 +36,74 @@ import javafx.stage.Stage;
  * reason: all keys are redirected to the popup scene's focusOwner - here
  * the table - which consumes some in its inputMap.
  * 
+ * Tentative solution: replace the table's eventDispatcher to intercept
+ * events needed in the textField.
+ * 
  * @author Jeanette Winzenburg, Berlin
  */
 public class ViewPopupApplication extends Application {
 
-    TextField textField;
+    public static class InterceptingEventDispatcher implements EventDispatcher {
+        private BasicEventDispatcher original;
+        private Predicate<Event> interceptor;
+        
+        public InterceptingEventDispatcher(BasicEventDispatcher original, Predicate<Event> interceptor) {
+            this.original = original;
+            this.interceptor = interceptor;
+        }
+
+        @Override
+        public Event dispatchEvent(Event event, EventDispatchChain tail) {
+            if (!interceptor.test(event)) {
+                event = original.dispatchCapturingEvent(event);
+                if (event.isConsumed()) {
+                    return null;
+                }
+            }
+            event = tail.dispatchEvent(event);
+            if (event != null && !interceptor.test(event)) {
+                event = original.dispatchBubblingEvent(event);
+                if (event.isConsumed()) {
+                    return null;
+                }
+            }
+            return event;
+        }
+       
+    }
     
     private Parent createContent() {
-        Popup popUp = new Popup();
-        TableView<Object> table = new TableView<>();
-        table.setFocusTraversable(false);
+        TableView<Locale> table = new TableView<>(FXCollections.observableArrayList(Locale.getAvailableLocales()));
+        // just to see that right/left are intercepted while up/down are handled
+        table.getSelectionModel().setCellSelectionEnabled(true);
         
-        TextField textField = new TextField() {
-
-            @Override
-            public EventDispatchChain buildEventDispatchChain(
-                    EventDispatchChain tail) {
-                if (popUp.isShowing()) {
-                    String dummy = "nothing";
-                }
-                return super.buildEventDispatchChain(tail);
+        TableColumn<Locale, String> country = new TableColumn<>("Country");
+        country.setCellValueFactory(new PropertyValueFactory<>("displayCountry"));
+        TableColumn<Locale, String> language = new TableColumn<>("Language");
+        language.setCellValueFactory(new PropertyValueFactory<>("displayLanguage"));
+        table.getColumns().addAll(country, language);
+        // disables default focus traversal
+        //  table.setFocusTraversable(false);
+        
+        // decide which keys to intercept
+        List<KeyCode> toIntercept = List.of(KeyCode.LEFT, KeyCode.RIGHT);
+        Predicate<Event> interceptor = e -> {
+            if (e instanceof KeyEvent) {
+                return toIntercept.contains(((KeyEvent) e).getCode());
             }
-            
+            return false;
         };
+        table.setEventDispatcher(new InterceptingEventDispatcher(
+                (BasicEventDispatcher) table.getEventDispatcher(), interceptor));
+        
+        TextField textField = new TextField("something to show");
         textField.setPrefColumnCount(20);
-        
-        
+        textField.setText("something to see");
 
         table.prefWidthProperty().bind(textField.widthProperty());
+        Popup popUp = new Popup();
         popUp.getContent().add(table);
-//        popUp.setHideOnEscape(true);
-//        popUp.setAutoHide(true);
         
-        
-
-        textField.setText("something to see");
-        // To see if the KeyEvent is triggered
-        textField.addEventFilter(KeyEvent.ANY, e -> {
-            System.out.println("got key: " + e);  
-            if (popUp.isShowing() && e.getCode() == LEFT && e.getEventType() == KEY_PRESSED) {
-                
-            }
-        });
-                
-//                System.out::println);
-
         textField.setOnKeyTyped(event -> {
             if(!popUp.isShowing()){
                 popUp.show(
